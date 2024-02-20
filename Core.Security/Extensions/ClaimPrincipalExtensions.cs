e709456041d1046fd60f3965379c85c19429055f
Ahmetkcak <ahmet.aliberk07@gmail.com>
2024-02-20 20:00:11 +0300

    add Authorization Behavior

--- Core.Security/Extensions/ClaimPrincipalExtensions.cs
+++ Core.Security/Extensions/ClaimPrincipalExtensions.cs
@@ -15,7 +15,7 @@ public static class ClaimPrincipalExtensions
         return result;
     }
 
-    public static List<string>? ClaimRole(this ClaimsPrincipal claimsPrincipal) => claimsPrincipal?.Claims(ClaimTypes.Role);
+    public static List<string>? ClaimRoles(this ClaimsPrincipal claimsPrincipal) => claimsPrincipal?.Claims(ClaimTypes.Role);
 
     public static int GetUserId(this ClaimsPrincipal claimsPrincipal) => Convert.ToInt32(claimsPrincipal?.Claims(ClaimTypes.NameIdentifier)?.FirstOrDefault());
 
